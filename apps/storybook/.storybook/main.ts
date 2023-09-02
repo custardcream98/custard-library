c69339ed360f4f3577f4fd74f448e4572dcaa1ea
shiwoo.park <custardcream@kakao.com>
2023-09-03 01:33:45 +0900

    feat(storybook): 스토리북 앱 추가

---
+++ apps/storybook/.storybook/main.ts
@@ -0,0 +1,34 @@
+import { join, dirname } from "path";
+import { mergeConfig } from "vite";
+import { type StorybookConfig } from "@storybook/react-vite";
+
+function getAbsolutePath(value: string) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+
+const config: StorybookConfig = {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-interactions"),
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  viteFinal: async (config) => {
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          "@custardcream/react-gantt": join(__dirname, "../../../packages/react-gantt/src"),
+        },
+      },
+    });
+  },
+};
+export default config;
