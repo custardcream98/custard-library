9b0733ce87cc216d39ede1b958d0dd590af859ce
shiwoo.park <custardcream@kakao.com>
2023-10-04 23:38:00 +0900

    feat(co-account): supabase 테스트

--- apps/co-account/src/app/page.tsx
+++ apps/co-account/src/app/page.tsx
@@ -1,10 +1,16 @@
 import { css } from "@style/css";
 
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
 const style = css({
   backgroundColor: "blue.200",
   minHeight: "100vh",
 });
 
-export default function Home() {
-  return <main className={style}></main>;
+export default async function HomePage() {
+  const supabase = createServerComponentClient({ cookies });
+  const { data } = await supabase.from("test").select();
+
+  return <main className={style}>{JSON.stringify(data)}</main>;
 }
