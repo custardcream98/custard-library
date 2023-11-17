82fb0538c0e0217f1e35bec703a2464b5c5e00fc
shiwoo.park <custardcream@kakao.com>
2023-11-18 02:29:17 +0900

    feat(web): mdx 사용

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,9 +1,15 @@
+import style from "./layout.module.scss";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body>
-        <header>
+      <body className={style.body}>
+        <header className={style.header}>
           <h1>Custard Library</h1>
+          <nav className={style.nav}>
+            <a href="/">홈으로</a>
+            <a href="/sample">샘플 보기</a>
+          </nav>
         </header>
         <main>
           <section>
