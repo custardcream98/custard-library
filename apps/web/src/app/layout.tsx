dd71462b1c7292afac68deb8edb983180686b837
shiwoo.park <custardcream@kakao.com>
2023-11-18 03:15:59 +0900

    feat(web): 기본적인 페이지 구성

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,22 +1,16 @@
 import style from "./layout.module.scss";
 
+import "../style/global.scss";
+import { Description, Header } from "./_component";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body className={style.body}>
-        <header className={style.header}>
-          <h1>Custard Library</h1>
-          <nav className={style.nav}>
-            <a href="/">홈으로</a>
-            <a href="/sample">샘플 보기</a>
-          </nav>
-        </header>
-        <main>
-          <section>
-            <h2>설명</h2>
-            <p>가장 최근 실험중인 라이브러리를 소개합니다.</p>
-          </section>
-          {children}
+        <Header />
+        <main className={style.main}>
+          <Description className={style.description} />
+          <div className="mdx">{children}</div>
         </main>
       </body>
     </html>
---
+++ apps/web/src/type.ts
@@ -0,0 +1,1 @@
+export type PropsWithClassName<T = {}> = T & { className?: string };
