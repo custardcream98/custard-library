6cc7cfe94da9402770d5caa9cee17fc89a603228
shiwoo.park <custardcream@kakao.com>
2023-11-18 22:55:40 +0900

    refactor(web): StoreViewer 컴포넌트화

--- apps/web/src/app/sample/_component/Sample/Sample.tsx
+++ apps/web/src/app/sample/_component/Sample/Sample.tsx
@@ -4,16 +4,15 @@ import {
   addStoreNode,
   addStoreSelectorNode,
   StoreRoot,
-  useCurrentStoreState_ONLY_FOR_DEVELOPMENT,
   useStoreNode,
   useStoreNodeGetter,
   useStoreSelectorNode,
 } from "@custardcream/very-simple-store";
 import React from "react";
 import { useRenderBlink } from "./useRenderBlink";
-import { PrettyPrintJSON } from "../PrettyPrintJSON";
 
 import style from "./Sample.module.scss";
+import { StoreViewer } from "./StoreViewer";
 
 export function Sample() {
   return (
@@ -29,39 +28,6 @@ export function Sample() {
         </div>
       </div>
     </StoreRoot>
-  );
-}
-
-const plainObjectFilter = (filters: string[], object: Record<any, any>) =>
-  Object.entries(object).reduce<Record<any, any>>((acc, [key, value]) => {
-    if (filters.includes(key)) return acc;
-    acc[key] = value;
-    return acc;
-  }, {});
-
-function StoreViewer() {
-  const store = useCurrentStoreState_ONLY_FOR_DEVELOPMENT();
-
-  const normalizedStore = React.useMemo(() => {
-    return {
-      selectors: Object.entries(store.selectors).reduce<Record<any, any>>((acc, [key, value]) => {
-        acc[key] = plainObjectFilter(["_dependencies", "_subscribers"], value);
-        return acc;
-      }, {}),
-      state: Object.entries(store.state).reduce<Record<any, any>>((acc, [key, value]) => {
-        acc[key] = plainObjectFilter(["_dependencies", "_subscribers"], value);
-        return acc;
-      }, {}),
-    };
-  }, [store]);
-
-  return (
-    <div className={style.wrapper}>
-      <div className={style.title}>Store의 현재 상태</div>
-      <div className={style.prettyPrintJsonWrapper}>
-        <PrettyPrintJSON json={normalizedStore} />
-      </div>
-    </div>
   );
 }
 
---
+++ apps/web/src/app/sample/_component/Sample/StoreViewer/StoreViewer.tsx
@@ -0,0 +1,38 @@
+import { useCurrentStoreState_ONLY_FOR_DEVELOPMENT } from "@custardcream/very-simple-store";
+import React from "react";
+
+import style from "./StoreViewer.module.scss";
+import { PrettyPrintJSON } from "../../PrettyPrintJSON";
+
+const plainObjectFilter = (filters: string[], object: Record<any, any>) =>
+  Object.entries(object).reduce<Record<any, any>>((acc, [key, value]) => {
+    if (filters.includes(key)) return acc;
+    acc[key] = value;
+    return acc;
+  }, {});
+
+export function StoreViewer() {
+  const store = useCurrentStoreState_ONLY_FOR_DEVELOPMENT();
+
+  const normalizedStore = React.useMemo(() => {
+    return {
+      selectors: Object.entries(store.selectors).reduce<Record<any, any>>((acc, [key, value]) => {
+        acc[key] = plainObjectFilter(["_dependencies", "_subscribers"], value);
+        return acc;
+      }, {}),
+      state: Object.entries(store.state).reduce<Record<any, any>>((acc, [key, value]) => {
+        acc[key] = plainObjectFilter(["_dependencies", "_subscribers"], value);
+        return acc;
+      }, {}),
+    };
+  }, [store]);
+
+  return (
+    <div className={style.wrapper}>
+      <div className={style.title}>Store의 현재 상태</div>
+      <div className={style.prettyPrintJsonWrapper}>
+        <PrettyPrintJSON json={normalizedStore} />
+      </div>
+    </div>
+  );
+}
