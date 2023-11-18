f8c774a67779c086db6641781076d69913cea67e
shiwoo.park <custardcream@kakao.com>
2023-11-18 17:55:59 +0900

    feat(web): 레포지토리 링크 추가

--- apps/web/src/mdx-components.tsx
+++ apps/web/src/mdx-components.tsx
@@ -1,7 +1,10 @@
 import type { MDXComponents } from "mdx/types";
 
+import style from "./mdx-components.module.scss";
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    a: (props) => <a className={style.link} {...props} target="_blank" rel="noopener noreferrer" />,
     ...components,
   };
 }
