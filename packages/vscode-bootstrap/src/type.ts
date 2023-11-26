c81a24e3f5a71f2c6225744bdac8a5a3e79867fe
shiwoo.park <custardcream@kakao.com>
2023-11-27 02:41:07 +0900

    feat(vscode-bootstrap): display 관련 토큰 추가

--- packages/vscode-bootstrap/src/type.ts
+++ packages/vscode-bootstrap/src/type.ts
@@ -3,4 +3,13 @@ export type SpacingToken = "m" | "p";
 export type DirectionToken = "t" | "b" | "l" | "r" | "x" | "y" | "";
 export type Direction = "top" | "bottom" | "left" | "right" | null;
 
+/**
+ * TODO: add breakpoint
+ */
+export type DisplayBaseToken = "d";
+
+export type DisplayType = "block" | "inline" | "inline-block" | "flex" | "inline-flex" | "none";
+
+export type DisplayToken = `${DisplayBaseToken}-${DisplayType}`;
+
 export type Level = 0 | 1 | 2 | 3 | 4 | 5 | "auto";
